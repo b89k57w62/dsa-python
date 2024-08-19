d91aed974faf58910c3fe9d88f7373b7b4028ca5
Jeffrey Wu <wu19930628@gmail.com>
2024-08-19 14:18:33 +0800

    recap: 19Aug 6kyu*2

--- war.py
+++ war.py
@@ -19,3 +19,15 @@ def tribonacci(signature, n):
 def count_bits(n):
    bin_num = format(n, 'b')
    return bin_num.count('1')
+
+# 6kyu Who likes it?
+def likes(names):
+   if len(names) == 0:
+      return "no one likes this"
+   elif len(names) == 1:
+      return f"{names[0]} likes this"
+   elif len(names) == 2:
+      return f"{names[0]} and {names[1]} like this"
+   elif len(names) == 3:
+      return f"{names[0]}, {names[1]} and {names[2]} like this"
+   return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
