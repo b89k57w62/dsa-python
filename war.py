e4a139b25c33b280dc29ef0c4eaf9a4ed1a62a37
Jeffrey Wu <wu19930628@gmail.com>
2024-09-04 16:36:36 +0800

    format: formatting war.py with black

--- war.py
+++ war.py
@@ -1,136 +1,153 @@
 # 6kyu Delete occurrences of an element if it occurs more than n times
-def delete_nth(order,max_e):
-   result = []
-   for num in order:
-      if result.count(num) < max_e:
-         result.append(num)
-   return result
+def delete_nth(order, max_e):
+    result = []
+    for num in order:
+        if result.count(num) < max_e:
+            result.append(num)
+    return result
+
 
 # 6kyu Tribonacci Sequence
 def tribonacci(signature, n):
-   if n <= 3:
-      return signature[:n]
-   result = signature[:]
-   for i in range(3, n):
-      result.append(sum(result[-3:]))
-   return result
+    if n <= 3:
+        return signature[:n]
+    result = signature[:]
+    for i in range(3, n):
+        result.append(sum(result[-3:]))
+    return result
+
 
 # 6kyu Bit Counting
 def count_bits(n):
-   bin_num = format(n, 'b')
-   return bin_num.count('1')
+    bin_num = format(n, "b")
+    return bin_num.count("1")
+
 
 # 6kyu Who likes it?
 def likes(names):
-   if len(names) == 0:
-      return "no one likes this"
-   elif len(names) == 1:
-      return f"{names[0]} likes this"
-   elif len(names) == 2:
-      return f"{names[0]} and {names[1]} like this"
-   elif len(names) == 3:
-      return f"{names[0]}, {names[1]} and {names[2]} like this"
-   return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return f"{names[0]} likes this"
+    elif len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
+
 
 # 6kyu Find the odd int
 def find_it(seq):
-   from collections import Counter
-   count_num = Counter(seq)
-   for key in count_num:
-      if count_num[key] % 2 == 1:
-         return key
+    from collections import Counter
+
+    count_num = Counter(seq)
+    for key in count_num:
+        if count_num[key] % 2 == 1:
+            return key
+
 
 # 5kyu Simple Pig Latin
 def pig_it(text):
-   ans = []
-   for str in text.split(' '):
-      if str.isalpha():
-         ans.append(str.replace(str[0], "", 1)+str[0]+"ay")
-      else:
-         ans.append(str)
-   return " ".join(ans)
+    ans = []
+    for str in text.split(" "):
+        if str.isalpha():
+            ans.append(str.replace(str[0], "", 1) + str[0] + "ay")
+        else:
+            ans.append(str)
+    return " ".join(ans)
+
 
 # 6kyu Array.diff
 def array_diff(a, b):
-   ans = []
-   for num in a:
-      if num not in b:
-         ans.append(num)
-   return ans
-   # solution2
-   # return [num for num in a if num not in b]
+    ans = []
+    for num in a:
+        if num not in b:
+            ans.append(num)
+    return ans
+    # solution2
+    # return [num for num in a if num not in b]
+
 
 # 7kyu Jaden Casing Strings
 def to_jaden_case(string):
-   return " ".join([words.capitalize() for words in string.split(" ")])
+    return " ".join([words.capitalize() for words in string.split(" ")])
+
 
 # 5kyu Not very secure
 def alphanumeric(password):
-   if len(password) == 0:
-      return False
-   ans = []
-   for char in password:
-      if char.isalpha() or char.isnumeric():
-         ans.append(char)
-   return True if len("".join(ans)) == len(password) else False
-   # solution2
-   # return password.isalnum() 
+    if len(password) == 0:
+        return False
+    ans = []
+    for char in password:
+        if char.isalpha() or char.isnumeric():
+            ans.append(char)
+    return True if len("".join(ans)) == len(password) else False
+    # solution2
+    # return password.isalnum()
+
 
 # 6kyu Stop gninnipS My sdroW!
 def spin_words(sentence):
-   array = sentence.split(" ")
-   return " ".join([words[::-1] if len(words) >=5 else words for words in array])
+    array = sentence.split(" ")
+    return " ".join([words[::-1] if len(words) >= 5 else words for words in array])
+
 
 # 6kyu Which are in?
 def in_array(array1, array2):
-   ans = []
-   for i in range(len(array1)):
-      for words in array2:
-         if array1[i] in words:
-            ans.append(array1[i])
-   return sorted(list(set(ans)))
+    ans = []
+    for i in range(len(array1)):
+        for words in array2:
+            if array1[i] in words:
+                ans.append(array1[i])
+    return sorted(list(set(ans)))
+
 
 # 6kyu Break camelCase
 def solution(s):
-   return "".join([" " + char if char.isupper() else char for char in s])
+    return "".join([" " + char if char.isupper() else char for char in s])
+
 
 # 5kyu Pete, the baker
 def cakes(recipe, available):
-   ans = []
-   if recipe.keys() <= available.keys():
-      for key in recipe:
-         ans.append(available[key] // recipe[key])
-      return min(ans)
-   return False
-   # solution2
-   # return min(available.get(k, 0)//recipe[k] for k in recipe)
+    ans = []
+    if recipe.keys() <= available.keys():
+        for key in recipe:
+            ans.append(available[key] // recipe[key])
+        return min(ans)
+    return False
+    # solution2
+    # return min(available.get(k, 0)//recipe[k] for k in recipe)
+
 
 # 5kyu Human Readable Time
 def make_readable(seconds):
-   hrs = seconds // 3600
-   mins = (seconds % 3600) // 60
-   secs = (seconds % 3600) % 60
-   return f"{hrs:02}:{mins:02}:{secs:02}"
+    hrs = seconds // 3600
+    mins = (seconds % 3600) // 60
+    secs = (seconds % 3600) % 60
+    return f"{hrs:02}:{mins:02}:{secs:02}"
+
 
 # 6kyu Equal Sides Of An Array
 def find_even_index(arr):
-   for i in range(len(arr)):
-      if sum(arr[0:i+1]) == sum(arr[i:]):
-         return i
-   return -1
+    for i in range(len(arr)):
+        if sum(arr[0 : i + 1]) == sum(arr[i:]):
+            return i
+    return -1
+
 
 # 6kyu The Supermarket Queue
 def queue_time(customers, n):
-   if customers == 0:
-      return 0
-   cashers = [0] * n
-   for customer in customers:
-      min_index = cashers.index(min(cashers))
-      cashers[min_index] += customer
-   return max(cashers)
+    if customers == 0:
+        return 0
+    cashers = [0] * n
+    for customer in customers:
+        min_index = cashers.index(min(cashers))
+        cashers[min_index] += customer
+    return max(cashers)
+
 
 # 6kyu Convert string to camel case
 def to_camel_case(text):
-   array = [char for char in text.replace("_", "-").split("-")]
-   ans = array[0] + "".join([char.capitalize() for char in array if char != array[0]])
-   return ans
+    array = [char for char in text.replace("_", "-").split("-")]
+    ans = array[0] + "".join([char.capitalize() for char in array if char != array[0]])
+    return ans
