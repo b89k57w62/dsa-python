3a2c2e0f2c1d31b4d155ce9dc81e528bfcfdc965
Jeffrey Wu <wu19930628@gmail.com>
2024-10-17 13:23:03 +0800

    update: 17OCT leetcode easy

--- war.py
+++ war.py
@@ -280,3 +280,19 @@ class Solution(object):
                 nums.remove(val)
         k = len(nums)
         return k
+
+
+# leetcode easy Remove Duplicates from Sorted Array
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_ptr = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[unique_ptr]:
+                unique_ptr += 1
+                nums[unique_ptr] = nums[i]
+
+        return unique_ptr + 1
