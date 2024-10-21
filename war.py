102fd08c0d5e976c95ef2f2a2440c9d4636bb278
Jeffrey Wu <wu19930628@gmail.com>
2024-10-21 16:11:08 +0800

    update: 21OCT leetcode easy

--- war.py
+++ war.py
@@ -345,3 +345,20 @@ class Solution(object):
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
+
+
+# leetcode easy Best Time to Buy and Sell Stock
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        min_price = float("inf")
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        return max_profit
