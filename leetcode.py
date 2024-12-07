1511c86c20d9f8253610d237b089c4c71da8fcea
Jeffrey Wu <wu19930628@gmail.com>
2024-12-07 14:41:50 +0800

    Update: 07Dec leetcode easy

--- leetcode.py
+++ leetcode.py
@@ -205,3 +205,22 @@ class Solution(object):
             else:
                 return False
         return True
+
+# leetcode easy Isomorphic Strings
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        map_s_t = {}
+        map_t_s = {}
+        for c1, c2 in zip(s, t):
+            if (c1 in map_s_t and map_s_t[c1] != c2) or (c2 in map_t_s and map_t_s[c2] != c1):
+                return False
+            map_s_t[c1] = c2
+            map_t_s[c2] = c1
+        return True
