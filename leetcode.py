da2a314a0fd10c986af3e62dface8939255dfd73
Jeffrey Wu <wu19930628@gmail.com>
2025-06-17 12:22:22 +0800

    update/ 17JUN leedcode 232 and quque

--- leetcode.py
+++ leetcode.py
@@ -636,3 +636,30 @@ class Solution:
             else:
                 self.stack.append(int(token))
         return self.stack.pop()
+
+
+# leetcode medium 232. Implement Queue using Stacks
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self._move()
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        self._move()
+        return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.out_stack and not self.in_stack
+
+    def _move(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
