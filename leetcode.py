ad5ce86a6de4d9129fd16d8333bda3e30d769d2f
Jeffrey Wu <wu19930628@gmail.com>
2025-06-25 14:43:07 +0800

    update/ 25JUN leetcode 110

--- leetcode.py
+++ leetcode.py
@@ -904,3 +904,22 @@ class Solution:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+# leetcode easy 110. Balanced Binary Tree
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root) >= 0
+
+    def dfs(self, root):
+        if root is None:
+            return 0
+        left_height = self.dfs(root.left)
+        if left_height < 0:
+            return -1
+        right_height = self.dfs(root.right)
+        if right_height < 0:
+            return -1
+        if abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height, right_height) + 1
