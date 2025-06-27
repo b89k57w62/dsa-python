3c83d5de0a3122b559cbe848d10fccbe651c1762
Jeffrey Wu <wu19930628@gmail.com>
2025-06-27 12:14:31 +0800

    update/ 27JUN leetcode 102 and bfs implemented

--- leetcode.py
+++ leetcode.py
@@ -968,3 +968,24 @@ class Solution:
         node.left = self._build(low, mid - 1, nums)
         node.right = self._build(mid + 1, high, nums)
         return node
+
+
+# leetcode medium 102. Binary Tree Level Order Traversal
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = deque()
+        queue.append(root)
+        res = []
+        while queue:
+            level_vals = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level_vals.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level_vals)
+        return res
