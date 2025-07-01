7c5bcd145a4e3a04639f450e69327e6b8dc819c2
Jeffrey Wu <wu19930628@gmail.com>
2025-07-01 16:25:47 +0800

    feature/ add leetcode 701 Insert into a Binary Search Tree

--- leetcode.py
+++ leetcode.py
@@ -1083,3 +1083,15 @@ class Solution:
         if not root:
             return []
         return self.in_order(root.left) + [root.val] + self.in_order(root.right)
+
+
+# leetcode medium 701. Insert into a Binary Search Tree
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
