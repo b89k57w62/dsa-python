3bff67c23c041738019904ca00febe793bd5c111
Jeffrey Wu <wu19930628@gmail.com>
2025-07-05 15:25:29 +0800

    feat: add solution for leetcode 912

--- leetcode.py
+++ leetcode.py
@@ -1169,3 +1169,30 @@ class Solution:
         right = self.build(mid + 1, hi)
         root.right = right
         return root
+
+
+# leetcode medium 912. Sort an Array
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        merged = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+        if i < len(left):
+            merged.extend(left[i:])
+        else:
+            merged.extend(right[j:])
+        return merged
