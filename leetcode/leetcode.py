8a23fb9f01d41c9845f073cbefcc777def68e6e0
Jeffrey Wu <wu19930628@gmail.com>
2025-07-24 14:36:23 +0800

    feat(leetcode): add solution for 56. Merge Intervals

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -1599,3 +1599,19 @@ class Solution:
             return 1
         else:
             return 0
+
+
+# leetcode medium 56. Merge Intervals
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) <= 1:
+            return intervals
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            if not merged or interval[0] > merged[-1][1]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
