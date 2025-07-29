0c55b4f50582efcf77d6851cdb68ce6b6142f803
Jeffrey Wu <wu19930628@gmail.com>
2025-07-29 20:49:50 +0800

    feat: add solution for Longest Consecutive Sequence

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -1707,3 +1707,22 @@ class Solution:
                 cols[col].add(num)
                 boxes[box_key].add(num)
         return True
+
+
+# leetcode medium 128. Longest Consecutive Sequence
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest_streak = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                start = num
+                current_streak = 1
+
+                while (start + 1) in num_set:
+                    start += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+        return longest_streak
