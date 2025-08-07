9914bcc18332b4217c93f64ee0732cd7e47e60b5
Jeffrey Wu <wu19930628@gmail.com>
2025-08-07 16:08:46 +0800

    feat: add two sum II solution for sorted array (leetcode 167)

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -1790,3 +1790,18 @@ class Solution:
             right_pointer -= 1
 
         return True
+
+
+# leetcode medium 167. Two Sum II - Input Array Is Sorted
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left_pointer, right_pointer = 0, len(numbers) - 1
+        while left_pointer <= right_pointer:
+            current_sum = numbers[left_pointer] + numbers[right_pointer]
+
+            if current_sum == target:
+                return [left_pointer + 1, right_pointer + 1]
+            elif current_sum < target:
+                left_pointer += 1
+            else:
+                right_pointer -= 1
