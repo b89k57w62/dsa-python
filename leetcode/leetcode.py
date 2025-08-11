5e464a746b00cfd88afd5aea27d3b8669f68c1f9
Jeffrey Wu <wu19930628@gmail.com>
2025-08-11 21:58:18 +0800

    refactor: optimize threeSum solution for better performance
    - Use enumerate for clearer indexing
    
    - Simplify sum comparison logic
    
    - Improve pointer updates for duplicate numbers

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -1611,30 +1611,26 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
-
-        for i in range(len(nums) - 2):
-            if nums[i] > 0:
-                break
-            if i > 0 and nums[i] == nums[i - 1]:
+        for idx, num in enumerate(nums):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
                 continue
-
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
+            left_idx, right_idx = idx + 1, len(nums) - 1
+            while left_idx < right_idx:
+                if nums[left_idx] + nums[right_idx] < -nums[idx]:
+                    left_idx += 1
+                elif nums[left_idx] + nums[right_idx] > -nums[idx]:
+                    right_idx -= 1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
+                    res.append([nums[idx], nums[left_idx], nums[right_idx]])
+                    while left_idx < right_idx and nums[left_idx] == nums[left_idx + 1]:
+                        left_idx += 1
+                    while (
+                        left_idx < right_idx and nums[right_idx] == nums[right_idx - 1]
+                    ):
+                        right_idx -= 1
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    left += 1
-                    right -= 1
+                    left_idx += 1
+                    right_idx -= 1
         return res
 
 
