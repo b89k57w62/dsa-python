e98a2d026a542eeca12885d6e8c95c592d2b7a28
Jeffrey Wu <wu19930628@gmail.com>
2025-08-21 16:58:00 +0800

    refactor: rename variable for clarity in coin_change_greedy

--- algorithm/greedy_algorithm/coin_change_greedy.py
+++ algorithm/greedy_algorithm/coin_change_greedy.py
@@ -4,13 +4,13 @@ def coin_change_greedy(coins: list[int], amount: int) -> int:
     time complexity: O(n)
     space complexity: O(1)
     """
-    end_idx = len(coins) - 1
+    idx = len(coins) - 1
     count = 0
     while amount > 0:
-        while end_idx >= 0 and coins[end_idx] > amount:
-            end_idx -= 1
-        if end_idx < 0:
+        while idx >= 0 and coins[idx] > amount:
+            idx -= 1
+        if idx < 0:
             return -1
-        amount -= coins[end_idx]
+        amount -= coins[idx]
         count += 1
     return count if amount == 0 else -1
