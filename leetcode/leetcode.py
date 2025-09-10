d56e7a32e461a0e83c3ce576f83ffaee6882020a
Jeffrey Wu <wu19930628@gmail.com>
2025-09-10 18:27:51 +0800

    feat(leetcode): add solution for Find the Duplicate Number (287)

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -2171,3 +2171,21 @@ class Solution:
             if l2:
                 l2 = l2.next
         return dummy.next
+
+
+# leetcode medium 287. Find the Duplicate Number
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        slow = 0
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
