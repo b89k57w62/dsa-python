0734a0d930e5feff9b3954e5f78ffde80b2c491b
Jeffrey Wu <wu19930628@gmail.com>
2025-11-26 10:18:33 +0800

    feat(leetcode): add solution for Partition Labels(763)

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -2663,3 +2663,20 @@ class Solution:
                 if triplet[i] == target[i]:
                     found.add(i)
         return len(found) == 3
+
+
+# leetcode medium 763. Partition Labels
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_idx = {}
+        for i, char in enumerate(s):
+            last_idx[char] = i
+        start = 0
+        end = 0
+        res = []
+        for i, char in enumerate(s):
+            end = max(end, last_idx[char])
+            if i == end:
+                res.append(i - start + 1)
+                start = i + 1
+        return res
