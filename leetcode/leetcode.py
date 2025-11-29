cf94fee02319c056e174af4c480d50bc64a949a0
Jeffrey Wu <wu19930628@gmail.com>
2025-11-29 12:54:22 +0800

    feat(leetcode): add solution for Valid Parenthesis String(678)

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -2698,3 +2698,27 @@ class Solution:
                 res.append(i - start + 1)
                 start = i + 1
         return res
+
+
+# leetcode medium 678. Valid Parenthesis String
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # min_range: The lowest possible number of open parentheses
+        # max_range: The highest possible number of open parentheses
+        min_range = 0
+        max_range = 0
+        for char in s:
+            if char == "(":
+                min_range += 1
+                max_range += 1
+            elif char == ")":
+                min_range -= 1
+                max_range -= 1
+            else:
+                min_range -= 1
+                max_range += 1
+            if max_range < 0:
+                return False
+            if min_range < 0:
+                min_range = 0
+        return min_range == 0
